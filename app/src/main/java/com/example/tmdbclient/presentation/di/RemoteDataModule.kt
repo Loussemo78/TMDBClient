321e125d46354701d9d50506fa70eae206d6f0e9
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-04-25 15:52:10 +0200

    RemoteDataModule added

---
+++ app/src/main/java/com/example/tmdbclient/presentation/di/RemoteDataModule.kt
@@ -0,0 +1,41 @@
+package com.example.tmdbclient.presentation.di
+
+import com.example.tmdbclient.data.model.api.TMDBService
+import com.example.tmdbclient.data.model.artist.repository.artist.datasource.ArtistRemoteDataSource
+import com.example.tmdbclient.data.model.artist.repository.artist.datasourceImpl.ArtistRemoteDataSourceImpl
+import com.example.tmdbclient.data.model.artist.repository.movie.datasource.MovieRemoteDataSource
+import com.example.tmdbclient.data.model.artist.repository.movie.datasourceImpl.MovieRemoteDataSourceImpl
+import com.example.tmdbclient.data.model.artist.repository.tvshow.datasource.TvShowRemoteDataSource
+import com.example.tmdbclient.data.model.artist.repository.tvshow.datasourceImpl.TvShowRemoteDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RemoteDataModule(private val apiKey:String) {
+
+    @Singleton
+    @Provides
+    fun provideMovieRemoteDatasource(tmdbService: TMDBService):MovieRemoteDataSource{
+        return MovieRemoteDataSourceImpl(
+            tmdbService, apiKey
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvRemoteDatasource(tmdbService: TMDBService):TvShowRemoteDataSource{
+        return TvShowRemoteDataSourceImpl(
+            tmdbService, apiKey
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistRemoteDatasource(tmdbService: TMDBService): ArtistRemoteDataSource {
+        return ArtistRemoteDataSourceImpl(
+            tmdbService, apiKey
+        )
+    }
+
+}
