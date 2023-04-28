eeaa66a8528976247fabb7f8e92d02156a5a130e
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-04-28 17:25:34 +0200

    update movies added on action bar

--- app/src/main/java/com/example/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/tmdbclient/presentation/movie/MovieActivity.kt
@@ -3,6 +3,9 @@ package com.example.tmdbclient.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -32,6 +35,35 @@ class MovieActivity : AppCompatActivity() {
        initRecyclerView()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this,Observer{
+            if (it != null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+            }
+        })
+    }
 
     private fun initRecyclerView(){
         binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
