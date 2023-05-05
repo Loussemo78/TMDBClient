6ddcf1da0f0c9a3c6ae5412b2ecfa883f0562ef0
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-05 21:43:05 +0200

    LiveDataTestUtil created

--- app/src/test/java/com/example/tmdbclient/presentation/movie/MovieViewModelTest.kt
+++ app/src/test/java/com/example/tmdbclient/presentation/movie/MovieViewModelTest.kt
@@ -2,13 +2,18 @@ package com.example.tmdbclient.presentation.movie
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.tmdbclient.data.model.movie.Movie
+import com.example.tmdbclient.data.repository.movie.FakeMovieRepository
+import com.example.tmdbclient.domain.usecase.GetMoviesUseCase
+import com.example.tmdbclient.domain.usecase.UpdateMoviesUseCase
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MovieViewModelTest{
+class MovieViewModelTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -17,6 +22,18 @@ class MovieViewModelTest{
 
     @Before
     fun setUp() {
+        val fakeMovieRepository = FakeMovieRepository()
+        val getMoviesUseCase = GetMoviesUseCase(fakeMovieRepository)
+        val updateMoviesUseCase = UpdateMoviesUseCase(fakeMovieRepository)
+        viewModel = MovieViewModel(getMoviesUseCase, updateMoviesUseCase)
+    }
 
+    @Test
+    fun getMovies_returnCurrentList(){
+         val movies = mutableListOf<Movie>()
+        movies.add(Movie(1,"overview1","path1","date1","title1"))
+        movies.add(Movie(2,"overview2","path2","date2","title2"))
+
+        val currentList = viewModel.getMovies()
     }
 }
